2acf90480d63b8af510662a8f9322c217ed5d953
= <=>
2015-12-29 17:01:35 +0300

    объявим необходимые функции

---
+++ actions.h
@@ -0,0 +1,17 @@
+#ifndef FSACTIONS
+#define FSACTIONS
+
+#define _FILE_OFFSET_BITS 64
+#define FUSE_USE_VERSION 26
+#include <fuse.h>
+
+
+#define FILESYSTEM "filesystem"
+#define NUMBER_OF_ROOT_BLOCK 0
+#define SIZE_OF_BLOCK 4096
+#define NODE_NAME_MAX_SIZE 256
+#define BLOCK_STATUS_OFFSET 0
+#define NODE_NAME_OFFSET (BLOCK_STATUS_OFFSET + sizeof(char))
+#define NODE_STAT_OFFSET (NODE_NAME_OFFSET + NODE_NAME_MAX_SIZE)
+#define NODE_CONTENT_OFFSET (NODE_STAT_OFFSET + sizeof(stat_t))
+#define NODE_CONTENT_MAX_SIZE (size_of_block - NODE_CONTENT_OFFSET)
